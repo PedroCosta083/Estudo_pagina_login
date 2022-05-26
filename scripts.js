fcad8965c7127e44e6b4dc8e9b5a45241bfaadae
Pedro Costa <pedroh.costa083@gmail.com>
2022-05-25 21:18:01 -0300

    Criando repositório
    criação do repositório

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    var cliquei = document.getElementById("nav-menu2");
+
+    if (cliquei.style.display == 'none') {
+        cliquei.style.display = 'flex';
+    } else {
+        cliquei.style.display = 'none';
+    }
+
+}
